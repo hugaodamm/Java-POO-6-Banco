2388afee95e128e055587019ae4cf066750aaae6
Hugo Damm <hugo2008rj@hotmail.com>
2021-08-31 15:08:46 -0300

    Initial commit

---
+++ src/com/company/Transacao.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Transacao {
+
+    public boolean transactionOk() {
+        return true;
+    }
+
+    public boolean transactionNoOk() {
+        return false;
+    }
+}
---
+++ src/com/company/banco.java
@@ -0,0 +1,44 @@
+package com.company;
+
+    public class banco extends Transacao {
+
+        public boolean deposito(String cliente){
+            if(cliente.equals("Executivo")){
+                return transactionOk();
+            }else{
+                return transactionNoOk();
+            }
+        }
+
+        public boolean transferencia(String cliente){
+            if(cliente.equals("Executivo")){
+                return transactionOk();
+            }else{
+                return transactionNoOk();
+            }
+        }
+
+        public boolean retirada(String cliente){
+            if(cliente.equals("basico") || cliente.equals("Coletores")){
+                return transactionOk();
+            }else{
+                return transactionNoOk();
+            }
+        }
+
+        public boolean consultaSaldo(String cliente){
+            if(cliente.equals("Basico") || cliente.equals("Coletores")){
+                return transactionOk();
+            }else{
+                return transactionNoOk();
+            }
+        }
+
+        public boolean pagamentoDeServicos(String cliente){
+            if(cliente.equals("Basico")){
+                return transactionOk();
+            }else{
+                return transactionNoOk();
+            }
+        }
+}
